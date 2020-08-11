0efb34c5a45c04a8670fbbc56fd5c82d5d539e8c
tedski999 <tedjohnsonjs@gmail.com>
2020-08-11 16:43:56 +0100

    Merged cutscreen.sh and cutscreensel.sh, added notification
    A notification is sent with a preview after the screenshot is taken.
    cutscreensel.sh has been replaced with cutscreen.sh sel.

--- .local/bin/cutscreen.sh
+++ .local/bin/cutscreen.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-maim | xclip -selection clipboard -t image/png
+SSPATH="/tmp/screenshot.png"
+
+if [[ "$1" == "" ]]; then
+	maim $SSPATH
+elif [[ "$1" == "sel" ]]; then
+	maim -uslc 1,1,1,0.05 $SSPATH
+else
+	echo "Usage: cutscreen [sel]" 2>&1
+	exit 1
+fi
+
+xclip $SSPATH -selection clipboard -t image/png
+dunstify -a "removeFromHistroy" -i $SSPATH "Screenshot saved to clipboard!" "$(identify -precision 4 -format '%b - %G %m' $SSPATH)"
 
--- .local/bin/cutscreensel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-maim -uslc 1,1,1,0.05 | xclip -selection clipboard -t image/png
-
