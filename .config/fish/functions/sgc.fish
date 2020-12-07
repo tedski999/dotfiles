83413691286c6afe73485f53cf4f03aab02f5f7a
tedski999 <tedjohnsonjs@gmail.com>
2020-12-07 01:54:18 +0000

    Couple of tweaks
    Replaced git user with tedski999 in septs ssh scripts
    Added function key support to i3
    Removed redundant Neovim shortcuts (replaced with sim links)
    Added popup to battery module in Polybar when clicked
    Added popup when attempting to focus a window from an empty scratchpad

--- .config/fish/functions/sgc.fish
+++ .config/fish/functions/sgc.fish
@@ -1,6 +1,6 @@
 function sgc --argument reponame
 	if set -q reponame[1]
-		git clone git@tjsj.dev:/srv/git/$reponame.git
+		git clone tedski999@tjsj.dev:/srv/git/$reponame.git
 	else
 		echo "sgc needs a repository name to clone from!" 1>&2
 	end
