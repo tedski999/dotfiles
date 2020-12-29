752211129640650b44d6b65d60e9646998ea37de
tedski999 <tedjohnsonjs@gmail.com>
2020-12-29 05:33:09 +0000

    Added irssi and cava
    Added asic irssi settings
    Configured cava console sound visualizer

---
+++ .config/fish/functions/irssi.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function irssi --wraps='irssi --home=~/.config/irssi/' --description 'alias irssi irssi --home=~/.config/irssi/'
+ command irssi --home=~/.config/irssi/ $argv;
+end
