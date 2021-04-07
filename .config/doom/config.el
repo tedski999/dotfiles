629b102c3bc9415c7d8f5a1fa427a88a2029cef9
tedski999 <tedjohnsonjs@gmail.com>
2021-04-07 01:22:13 +0100

    Added DOOM Emacs config

---
+++ .config/doom/config.el
@@ -0,0 +1,25 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+(setq user-full-name "Ted Johnson"
+      user-mail-address "tedjohnsonjs@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+(setq doom-theme 'doom-tomorrow-night)
+(setq org-directory "~/.local/share/org/")
+(setq display-line-numbers-type "relative")
+
