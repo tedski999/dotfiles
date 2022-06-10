ed4a971b3fe158e8cdd844a1c5015e84fdcef479
tedski999 <tedjohnsonjs@gmail.com>
2022-06-10 20:07:55 +0100

    Cleanup polybar updates.sh

--- .config/polybar/modules/updates.sh
+++ .config/polybar/modules/updates.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-trap "do_update" 10 # USR1
-function do_update {
-	$TERMINAL -e yay
-}
-
-trap "refresh" 12 # USR2
-function refresh {
-	: # Do nothing
-}
+trap "$TERMINAL -e yay" 10 # USR1 to do system update
+trap ":" 12 # USR2 to force refresh update counter
 
 while true; do
 	updates_arch="$(checkupdates 2>/dev/null | wc -l)"
